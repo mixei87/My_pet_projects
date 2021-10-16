1e44c12a00ec0f19170ba84dd661712733408bad
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 03:49:24 +0700

    3.94 Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,12 +1,21 @@
 from sys import stdin
-n = int(stdin.readline())
-p = {}
+n = int(stdin.readline().strip())
+p = []
 
 for k in range(n):
-    x1, y1, x2, y2 = [int(e) for e in stdin.readline().split()]
+    x1, y1, x2, y2 = list(map(int, stdin.readline().strip().split()))
     for i in range(x1, x2):
         for j in range(y1, y2):
-            p[str(i)+str(j)] = k
+            flag = True
+            for m in p:
+                if m[0] == i and m[1] == j:
+                    p.remove(m)
+                    p.append((i, j, k))
+                    flag = False
+                    break
+            if flag:
+                p.append((i, j, k))
 
+tmp = [i[2] for i in p]
 for i in range(n):
-    print(list(k for k in p.values()).count(i))
+    print(tmp.count(i))
