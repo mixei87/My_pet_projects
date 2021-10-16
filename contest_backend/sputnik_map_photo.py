4261b96bb7350c0a77b9867235155c8e5055b2cf
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 01:37:44 +0700

    514.43 Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -3,10 +3,10 @@ n = int(stdin.readline().strip())
 p = {}
 
 for k in range(n):
-    x1, y1, x2, y2 = list(map(int, stdin.readline().strip().split()))
+    x1, y1, x2, y2 = [int(n) for n in stdin.readline().split(" ")]
     for i in range(x1, x2):
         for j in range(y1, y2):
-            p[f'{i}{j}'] = k
+            p[str(i) + str(j)] = k
 
 for i in range(n):
     print(list(k for k in p.values()).count(i))
