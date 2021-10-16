8230d30a013b68a4d21c7b44df61bf4fb4d2f556
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 04:24:08 +0700

    12.041 s

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,18 +1,18 @@
 from sys import stdin
-n = int(stdin.readline().strip())
-val = []
+n = int(stdin.readline())
 key = []
 
 for k in range(n):
-    x1, y1, x2, y2 = list(map(int, stdin.readline().strip().split()))
+    x1, y1, x2, y2 = [int(e) for e in stdin.readline().split()]
     for i in range(x1, x2):
         for j in range(y1, y2):
             q = f'{i}{j}'
-            if q in key:
-                val[key.index(q)] = k
+            qq = [m for m in key if m[0] == q]
+            if len(qq) == 0:
+                key.append([q, k])
             else:
-                key.append(q)
-                val.append(k)
+                key[key.index(qq[0])] = [q, k]
 
+tmp = [i[1] for i in key]
 for i in range(n):
-    print(val.count(i))
+    print(tmp.count(i))
