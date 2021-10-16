78282a034d184015e5e3475e0ba2ca287790028d
Mikhail Perevalov <mixei87@mail.ru>
2021-10-16 21:28:27 +0700

    another decision

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -3,17 +3,24 @@ n = int(stdin.readline().strip())
 coordinates = [list(map(int, stdin.readline().strip().split()))
                for i in range(n)]
 
-photo_pixels = set()
-actual_number_square = []
+photo_pixels = {}
+N = n
 for k in reversed(coordinates):
     x1, y1, x2, y2 = k[0], k[1], k[2], k[3]
     count = 0
+    n -= 1
     for i in filter(lambda x: x != 0, range(x1, x2+1)):
         for j in filter(lambda y: y != 0, range(y1, y2+1)):
             if (i, j) not in photo_pixels:
-                photo_pixels.add((i, j))
                 count += 1
-    actual_number_square.append(count)
+                photo_pixels[(i, j)] = (n, count)
 
-for i in reversed(actual_number_square):
-    print(i)
+
+actual_number_square = {}
+for number, counter in photo_pixels.values():
+    res = actual_number_square.get(number)
+    if res == None or counter > res:
+        actual_number_square[number] = counter
+
+for i in range(N):
+    print(actual_number_square.get(i, 0))
