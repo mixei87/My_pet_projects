a72946917886997109d14bff05ebd58fb3b48cd4
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 01:00:15 +0700

    516.82 Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -5,8 +5,8 @@ co = []
 for m in range(n):
     co.append(list(map(int, stdin.readline().strip().split())))
 
-photo_pixels = {str(i)+str(j): k for k in range(n) for i in filter(lambda x: x != 0, range(
-    co[k][0], co[k][2]+1)) for j in filter(lambda y: y != 0, range(co[k][1], co[k][3]+1))}
+photo_pixels = {str(i)+str(j): k for k in range(n)
+                for i in range(co[k][0], co[k][2]) for j in range(co[k][1], co[k][3])}
 
 for i in range(n):
     print(list(k for k in photo_pixels.values()).count(i))
