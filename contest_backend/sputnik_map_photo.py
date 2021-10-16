eea6ac506ab403feb0d7bce773848470abd8afe0
Mikhail Perevalov <mixei87@mail.ru>
2021-10-16 22:48:36 +0700

    527.57 Mb :(

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -3,20 +3,12 @@ n = int(stdin.readline().strip())
 photo_pixels = {}
 
 for k in range(n):
-    x1, y1, x2, y2 = tuple(map(int, stdin.readline().strip().split()))
+    x1, y1, x2, y2 = list(map(int, stdin.readline().strip().split()))
     for i in range(x1, x2+1):
         for j in range(y1, y2+1):
             if i != 0 and j != 0:
                 photo_pixels[(i, j)] = k
 
-
-actual_number_square = {}
-for k in photo_pixels.values():
-    res = actual_number_square.get(k)
-    if res == None:
-        actual_number_square[k] = 1
-    else:
-        actual_number_square[k] = res + 1
-
+actual_number_square = [k for k in photo_pixels.values()]
 for i in range(n):
-    print(actual_number_square.get(i, 0))
+    print(actual_number_square.count(i))
