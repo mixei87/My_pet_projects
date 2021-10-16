9eb63be1d096be29d3831de210b1bc04eebce7e5
Mikhail Perevalov <mixei87@mail.ru>
2021-10-16 22:36:17 +0700

    don_t decrease memory usage

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,22 +1,22 @@
 from sys import stdin
 n = int(stdin.readline().strip())
-coord = []
 photo_pixels = {}
 
 for k in range(n):
-    coord.append(list(map(int, stdin.readline().strip().split())))
-    for i in filter(lambda x: x != 0, range(coord[k][0], coord[k][2]+1)):
-        for j in filter(lambda y: y != 0, range(coord[k][1], coord[k][3]+1)):
-            photo_pixels[(i, j)] = k
+    x1, y1, x2, y2 = tuple(map(int, stdin.readline().strip().split()))
+    for i in range(x1, x2+1):
+        for j in range(y1, y2+1):
+            if i != 0 and j != 0:
+                photo_pixels[(i, j)] = k
 
 
 actual_number_square = {}
-for number in photo_pixels.values():
-    res = actual_number_square.get(number)
+for k in photo_pixels.values():
+    res = actual_number_square.get(k)
     if res == None:
-        actual_number_square[number] = 1
+        actual_number_square[k] = 1
     else:
-        actual_number_square[number] = res + 1
+        actual_number_square[k] = res + 1
 
 for i in range(n):
     print(actual_number_square.get(i, 0))
