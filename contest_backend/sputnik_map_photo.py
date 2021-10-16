e2993791bfca86a05aa707b6966a01299b95c188
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 01:51:36 +0700

    516.91 Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,12 +1,16 @@
-from sys import stdin
-n = int(stdin.readline().strip())
+reader = open('input.txt', 'r')
+n = int(reader.readline().strip())
 p = {}
 
 for k in range(n):
-    x1, y1, x2, y2 = [int(n) for n in stdin.readline().split(" ")]
+    x1, y1, x2, y2 = [int(n) for n in reader.readline().split(" ")]
     for i in range(x1, x2):
         for j in range(y1, y2):
             p[str(i) + str(j)] = k
+reader.close()
 
+tmp = [k for k in p.values()]
+writer = open('output.txt', 'w')
 for i in range(n):
-    print(list(k for k in p.values()).count(i))
+    writer.write(f'{tmp.count(i)}\n')
+writer.close()
