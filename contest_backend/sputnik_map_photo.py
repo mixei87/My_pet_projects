8f584dcef2d22417757ec3de992f782129d6ad30
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 00:57:14 +0700

    515.76 Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,12 +1,12 @@
 from sys import stdin
 n = int(stdin.readline().strip())
-photo_pixels = {}
 
-for k in range(n):
-    x1, y1, x2, y2 = list(map(int, stdin.readline().strip().split()))
-    for i in filter(lambda x: x != 0, range(x1, x2+1)):
-        for j in filter(lambda y: y != 0, range(y1, y2+1)):
-            photo_pixels[str(i)+str(j)] = k
+co = []
+for m in range(n):
+    co.append(list(map(int, stdin.readline().strip().split())))
+
+photo_pixels = {str(i)+str(j): k for k in range(n) for i in filter(lambda x: x != 0, range(
+    co[k][0], co[k][2]+1)) for j in filter(lambda y: y != 0, range(co[k][1], co[k][3]+1))}
 
 for i in range(n):
     print(list(k for k in photo_pixels.values()).count(i))
