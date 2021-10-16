30f57523be8cb66ddfb2a1141a564aa28d9649c0
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 01:59:27 +0700

    512.49 Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -3,7 +3,7 @@ n = int(stdin.readline())
 p = {}
 
 for k in range(n):
-    x1, y1, x2, y2 = list(map(int, stdin.readline().split()))
+    x1, y1, x2, y2 = [int(e) for e in stdin.readline().split()]
     for i in range(x1, x2):
         for j in range(y1, y2):
             p[str(i)+str(j)] = k
