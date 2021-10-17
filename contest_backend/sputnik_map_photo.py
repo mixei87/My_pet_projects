402ac40b68cbcafc6e6c22713edcdd80d0a9a987
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 19:04:27 +0700

    12.002s 4.77Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,11 +1,26 @@
 from sys import stdin
 n = int(stdin.readline())
 
+
+class Photo:
+    __slots__ = ('coord', 'num')
+
+    def __init__(self):
+        self.coord = []
+        self.num = []
+
+
 t_c = [[int(e) for e in stdin.readline().split()] for _ in range(n)]
-del stdin
-photo = {str(i)+str(j): k for k in range(n) for i in range(
-    t_c[k][0], t_c[k][2]) for j in range(t_c[k][1], t_c[k][3])}
-del t_c
+photo = Photo()
+for k in range(n):
+    for i in range(t_c[k][0], t_c[k][2]):
+        for j in range(t_c[k][1], t_c[k][3]):
+            str_c = str(i)+str(j)
+            if str_c not in photo.coord:
+                photo.coord.append(str_c)
+                photo.num.append(k)
+            else:
+                photo.num[photo.coord.index(str_c)] = k
+
 for i in range(n):
-    print(len([photo.pop(key)
-          for key, val in list(photo.items()) if val == i]))
+    print(photo.num.count(i))
