25ec0e11c8bd362ee018167506bc7e774eb4295e
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 16:17:20 +0700

    12.049s 5.38 Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,18 +1,35 @@
+from dataclasses import dataclass
 from sys import stdin
 n = int(stdin.readline())
-key = []
 
+
+class Photo:
+    __slots__ = ('name', 'listPhoto')
+
+    def __init__(self, name=""):
+        self.name = name
+        self.listPhoto = []
+
+
+@dataclass
+class dataPhoto:
+    __slots__ = ('x', 'y', 'counter')
+    x: int
+    y: int
+    counter: int
+
+
+photo = Photo('mymap')
 for k in range(n):
     x1, y1, x2, y2 = [int(e) for e in stdin.readline().split()]
     for i in range(x1, x2):
         for j in range(y1, y2):
-            q = f'{i}{j}'
-            qq = [m for m in key if m[0] == q]
-            if len(qq) == 0:
-                key.append([q, k])
+            trt = [a for a in photo.listPhoto if a.x == i and a.y == j]
+            if len(trt) == 0:
+                photo.listPhoto.extend([dataPhoto(i, j, k)])
             else:
-                key[key.index(qq[0])] = [q, k]
+                trt[0].counter = k
 
-tmp = [i[1] for i in key]
+tmp = [i.counter for i in photo.listPhoto]
 for i in range(n):
     print(tmp.count(i))
