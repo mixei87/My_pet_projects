6e7c1049c4303dc8bb4283c2b0e000da81db7eb7
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 16:45:12 +0700

    3.838s 513.95Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,4 +1,5 @@
 from sys import stdin
+# from sys import getsizeof
 n = int(stdin.readline())
 
 
@@ -9,10 +10,12 @@ class Photo:
         self.listPhoto = {}
 
 
-t_c = tuple(int(e) for k in range(n) for e in stdin.readline().split())
+t_c = [[int(e) for e in stdin.readline().split()] for _ in range(n)]
 photo = Photo()
 photo.listPhoto = {f'{i}{j}': k for k in range(n) for i in range(
-    t_c[k*4], t_c[k*4 + 2]) for j in range(t_c[k*4 + 1], t_c[k*4 + 3])}
+    t_c[k][0], t_c[k][2]) for j in range(t_c[k][1], t_c[k][3])}
 
 for i in range(n):
     print([k for k in photo.listPhoto.values()].count(i))
+
+# print(getsizeof(photo))
