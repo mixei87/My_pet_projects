d849e5f34ba67358addc6f7ef8a39c2f0e9f1778
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 19:48:41 +0700

    12.072s Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -10,17 +10,16 @@ class Photo:
         self.num = []
 
 
-t_c = [[int(e) for e in stdin.readline().split()] for _ in range(n)]
+t_c = [[int(e) for e in stdin.readline().split()] for k in range(n)]
 photo = Photo()
-for k in range(n):
-    for i in range(t_c[k][0], t_c[k][2]):
-        for j in range(t_c[k][1], t_c[k][3]):
+for k in reversed(t_c):
+    counter = 0
+    for i in range(k[0], k[2]):
+        for j in range(k[1], k[3]):
             str_c = str(i)+str(j)
             if str_c not in photo.coord:
                 photo.coord.append(str_c)
-                photo.num.append(k)
-            else:
-                photo.num[photo.coord.index(str_c)] = k
-
-for i in range(n):
-    print(photo.num.count(i))
+                counter += 1
+    photo.num.append(counter)
+for i in reversed(photo.num):
+    print(i)
