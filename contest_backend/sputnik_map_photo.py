ce58ffaabda81b0e0176c4634a47ab2ece02731b
Mikhail Perevalov <mixei87@mail.ru>
2021-10-17 18:39:40 +0700

    516.52Mb

--- contest_backend/sputnik_map_photo.py
+++ contest_backend/sputnik_map_photo.py
@@ -1,10 +1,11 @@
 from sys import stdin
 n = int(stdin.readline())
 
-listPhoto = {}
 t_c = [[int(e) for e in stdin.readline().split()] for _ in range(n)]
-listPhoto = {str(i)+str(j): k for k in range(n) for i in range(
+del stdin
+photo = {str(i)+str(j): k for k in range(n) for i in range(
     t_c[k][0], t_c[k][2]) for j in range(t_c[k][1], t_c[k][3])}
-
+del t_c
 for i in range(n):
-    print([k for k in listPhoto.values()].count(i))
+    print(len([photo.pop(key)
+          for key, val in list(photo.items()) if val == i]))
