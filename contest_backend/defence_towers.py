2a6e1a40db5a32b3fb8cf4c82776eddb3a80e3ad
Mikhail Perevalov <mixei87@mail.ru>
2021-10-18 23:18:55 +0700

    1.026s 77.54 Mb

--- contest_backend/defence_towers.py
+++ contest_backend/defence_towers.py
@@ -1,26 +1,29 @@
 from sys import stdin
 
 n, m = [int(i) for i in stdin.readline().split()]
-a = [int(i) for i in stdin.readline().split()]
-b = [int(i) for i in stdin.readline().split()]
+a = tuple(reversed([int(i) for i in stdin.readline().split()]))
+b = tuple(sorted([int(i) for i in stdin.readline().split()], reverse=True))
 
-min_b = min(b)
+min_b = b[-1]
 max_shadow = 0
-sun_places = []
-for i in reversed(a):
-    len_sun = i-max_shadow
-    if len_sun >= min_b:
-        sun_places.append(len_sun)
+sun_places = {}
+for i in a:
+    len_shadow = i-max_shadow
+    if len_shadow >= min_b:
+        sun_places[len_shadow] = sun_places.get(len_shadow, 0) + 1
     if i > max_shadow:
         max_shadow = i
 
 count = 0
-for i in sorted(b, reverse=True):
+for i in b:
     if len(sun_places):
         max_sun_places = max(sun_places)
         if i <= max_sun_places:
             count += 1
-            sun_places.remove(max_sun_places)
+            if sun_places[max_sun_places] > 1:
+                sun_places[max_sun_places] -= 1
+            else:
+                del sun_places[max_sun_places]
     else:
         break
 print(count)
