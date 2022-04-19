cc7a07b91a9631121e58daeef0eb6e57bb9899f4
Mikhail Perevalov <mixei87@mail.ru>
2022-04-19 15:06:28 +0700

    not bad code

--- school21/python_intensive/Python_day00-0/src/blocks.py
+++ school21/python_intensive/Python_day00-0/src/blocks.py
@@ -1,21 +1,31 @@
 from sys import argv, stdin
+from argparse import ArgumentParser
 
-if len(argv) != 2:
-  print("Number of arguments isn't 1")
-  exit(1)
 
-n = int(argv[1])
-for string in stdin:
-  if (n <= 0):
-    exit(0)
-  string = string.rstrip()
-  if len(string) == 32:
-    flag = True
-    for j in range(5):
-      if string[j] != '0':
-        flag = False
-    if string[j+1] == '0':
-      flag = False
-    if flag == True:
+def CheckHash(hash_str: str) -> bool:
+  if len(hash_str) == 32 and hash_str[:5] == "00000" and hash_str != "0":
+    return True
+  return False
+
+
+def CheckInput(params: list):
+  if len(params) != 2:
+    print("Number of arguments isn't 1")
+    exit(1)
+
+
+def PrintStrings():
+  # check argument
+  n = int(argv[1])
+  for string in stdin:
+    if (n <= 0):
+      break
+    string = string.rstrip()
+    if CheckHash(string):
       print(string)
-  n -= 1
+    n -= 1
+
+
+if __name__ == '__main__':
+  CheckInput(argv)
+  PrintStrings()
