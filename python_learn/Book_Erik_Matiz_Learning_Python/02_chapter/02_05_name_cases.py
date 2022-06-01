575f87ed599bbb6efc66471c9eda41d796456b9a
Mikhail Perevalov <mixei87@mail.ru>
2022-06-01 09:28:37 +0700

    chapter2 complete

---
+++ python_learn/Book_Erik_Matiz_Learning_Python/02_chapter/02_05_name_cases.py
@@ -0,0 +1,2 @@
+if __name__ == '__main__':
+    print('Albert Einstein once said: "A person who never made a \'mistake\' never tried anything new."')
---
+++ python_learn/Book_Erik_Matiz_Learning_Python/02_chapter/02_06_name_cases.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    famous_person = 'Albert Einstein'
+    message = famous_person + \
+      ' once said: "A person who never made a \'mistake\' never tried anything new."'
+    print(message)
