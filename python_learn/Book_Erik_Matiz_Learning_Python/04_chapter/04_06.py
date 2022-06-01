47d7f21fefb93855f4fc5eaab5a4f232aa86580c
Mikhail Perevalov <mixei87@mail.ru>
2022-06-01 10:21:17 +0700

    chapter 05 complete

---
+++ python_learn/Book_Erik_Matiz_Learning_Python/04_chapter/04_06.py
@@ -0,0 +1,8 @@
+def main():
+    numbers = list(range(1, 20, 2))
+    for number in numbers:
+        print(number)
+
+
+if __name__ == "__main__":
+    main()
---
+++ python_learn/Book_Erik_Matiz_Learning_Python/04_chapter/04_09.py
@@ -0,0 +1,7 @@
+def main():
+    for number in [x**3 for x in range(1, 11)]:
+        print(number)
+
+
+if __name__ == "__main__":
+    main()
